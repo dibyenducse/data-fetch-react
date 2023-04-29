829f30522b8f5732110ed3d9c15fbbca873cc34b
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-04-29 16:51:39 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,2 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+
+export default function Quote() {
+    const [quote, setQuote] = useState(null);
+    
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+            
+                const response = await fetch('https://api.adviceslip.com/advice')
+                const data = await response.json();
+                const advice = data.slip.advice;
+                setQuote(advice);
+                console.log(advice)
+          
+        }
+            fetchWeather();
+
+        }, []);
+
+    return (
+        <>
+        <div>Get Api Data</div>
+            <div>{quote}</div>
+        </>
+    )
+}
